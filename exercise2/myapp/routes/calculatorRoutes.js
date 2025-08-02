ea35c8346c2fc2c8e1cbe3bc1d018c80378c4ce5
starkie001 <starkwanos@hotmail.com>
2025-08-02 16:44:11 +1200

    added subtract functionality

--- exercise2/myapp/routes/calculatorRoutes.js
+++ exercise2/myapp/routes/calculatorRoutes.js
@@ -11,4 +11,13 @@ router.get('/add', (req, res) => {
     res.json({result:sum})
 })
 
+router.get('/subtract', (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sum = number1 - number2;
+    console.log(sum)
+    res.status(200)
+    res.json({result:sum})
+})
+
 module.exports = router;
