e36bd37ecd8a9a292e862d4be639ae21a5c3f10c
starkie001 <starkwanos@hotmail.com>
2025-08-02 16:54:11 +1200

    exercise 2 divide and multiply functionality added

--- exercise2/myapp/routes/calculatorRoutes.js
+++ exercise2/myapp/routes/calculatorRoutes.js
@@ -20,4 +20,22 @@ router.get('/subtract', (req, res) => {
     res.json({result:sum})
 })
 
+router.get('/divide', (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let quotient = number1 / number2;
+    console.log(quotient)
+    res.status(200)
+    res.json({result:quotient})
+})
+
+router.get('/multiply', (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let product = number1 * number2;
+    console.log(product)
+    res.status(200)
+    res.json({result:product})
+})
+
 module.exports = router;
